797519771582a59c3d0af5f64372862225eba183
rajesh <nethunurirajesh@gmail.com>
2025-01-27 07:14:44 +0000

    added code

---
+++ src/test/java/com/ust/springShopping/SpringShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.springShopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
